b59ed237df703df4794fe7dd8041d7801ab7dd8b
Logan Prough <loganprough@ksu.edu>
2018-04-28 14:36:34 -0500

    Started find substring function

--- 3way-pthread/findsubstrings.c
+++ 3way-pthread/findsubstrings.c
@@ -3,20 +3,50 @@
 #include <string.h>
 #include <pthread.h>
 
+struct substring {
+  char *s;
+  int len;
+};
+
 int main(int argc, char **argv) {
   int err, numlines = atoi(argv[1]);
   FILE *fd;
   char **line = (char **)malloc(numlines * sizeof(char *));
-  char *buf = malloc(100000);
 
   // Inspired by find_keys.c in Dr. Andresen's home directory
   fd = fopen("/homes/dan/625/wiki_dump.txt", "r");
   for(int i = 0; i < numlines && err != EOF; i++) {
-    err = fscanf(fd, "%[^\n]\n", buf);
-    int l = strlen(buf);
-    line[i] = malloc(l * sizeof(char));
-    strncpy(line[i], buf, l);
+    line[i] = (char *)malloc(10000 * sizeof(char));
+    err = fscanf(fd, "%[^\n]\n", line[i]);
   }
   fclose(fd);
-  for(int i = 0; i < numlines; i++) printf("%s\n", line[i]);
+
+  //for(int i = 0; i < numlines; i++) printf("%s\n", line[i]);
+  
+  char a[6] = "ababc\0";
+  char b[6] = "babca\0";
+  char c[6] = "abcba\0";
+
+  printf("%s\n", lss(a, b));
+  printf("%s\n", lss(b, c));
+  printf("%s\n", lss(a, c));
+}
+
+char *lss(char *a, char *b) {
+  char *ta = a, *tb = b, *ea = a + strlen(a), *eb = b + strlen(b);
+  char len;
+  struct substring = malloc(sizeof(struct substring));
+
+  while(ta < ea) {
+    while(tb < b) {
+      len = 0;
+      while(*ta == *tb) {
+        len++;
+        ta++;
+        tb++
+      }
+      ta -= len + 1;
+      tb -= len + 1;
+    }
+  }     
 }
